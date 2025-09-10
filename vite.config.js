57bfceff99f23bb17b537592dd4ae2a923e1ba36
momo <dangvanquocmomo@gmail.com>
2025-09-10 09:28:41 +0700

    Add GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FurEver-Care/", // Thay "FurEver-Care" bằng tên repository của bạn
+  base: process.env.NODE_ENV === "production" ? "/FurEver-Care/" : "/", // Thay "FurEver-Care" bằng tên repository của bạn
   server: {
     port: 3000,
     open: true,
